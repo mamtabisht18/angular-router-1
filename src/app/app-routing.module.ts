aecd201579d70b0ca54ad96a4afc094fe494d297
“Mamta” <mamta.bisht@outlook.com>
2020-04-05 00:45:52 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,26 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AngularComponent } from './angular/angular.component';
+import { ReactComponent } from './react/react.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'courses', component: CoursesComponent,
+    children: [
+      { path: 'angular', component: AngularComponent },
+      { path: 'react', component: ReactComponent },
+    ]
+  },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AngularComponent } from './angular/angular.component';
+import { ReactComponent } from './react/react.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CoursesComponent,
+    AboutUsComponent,
+    AngularComponent,
+    ReactComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
